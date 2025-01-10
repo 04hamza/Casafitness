e14d41b51e60c51ab71e3328eb350d6cd6f64c48
Your Name <aitboubkerhamza18@gmail.com>
2025-01-10 09:00:44 +0100

    Updated CSS and JavaScript files for various components

--- src/FAQ/FAQ.js
+++ src/FAQ/FAQ.js
@@ -1,9 +1,36 @@
 import "./FAQ.css"
+import { Link } from "react-router-dom"
 const FAQ=()=>{
     return(
         <div className="FAQ">
             <img src="Images/FAQ.png"></img>
             <h2 className="title">How can we assist you?</h2>
+            <div className="aides">
+                <Link className="aidelink" to="/Activities">
+                <div className="aide">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
+                </svg>
+                <div className="titles">ACTIVITIES</div>
+                </div>
+                </Link>
+                <Link className="aidelink" to="/gyms">
+                <div className="aide">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+                <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>
+                </svg>
+                <div className="titles">CLUBS</div>
+                </div>
+                </Link>
+                <Link className="aidelink" to="">
+                <div className="aide">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ticket" viewBox="0 0 16 16">
+                <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5z"/>
+                </svg>
+                <div className="titles">SUBSCRIPTIONS</div>
+                </div>
+                </Link>
+            </div>
         </div>
     )
 }
--- src/Footer/Links.js
+++ src/Footer/Links.js
@@ -1,7 +1,7 @@
 const Links=[
     {titLe:"Rejoindre Fitness Park Group",links:["Propose a location","Become a Franchisee","Become a Master Franchisee","Special offer for businesses","Recruitment"]},
     {titLe:"Help & Information",links:["FAQ","Subscriptions","Want to try our gyms?","Contact"]},
-    {titLe:"Clubs",links:["New York City","Los Angeles","Chicago","Houston","Miami"]},
-    {titLe:"Activities",links:["Fight Park","Burning Park","Cycle Park","Cross training","Cardio training","Group classes","Weight training"]},
+    {titLe:"Clubs",links:["Casablanca","Marrakech","Tangier","Fes","Rabat"]},
+    {titLe:"Activities",links:["Musculation","Cross-training","Cardio-training","Cours_collectifs"]},
 ]
 export default Links
